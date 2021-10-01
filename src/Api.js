20750dbd74d1a33aa0de6dc9bf0c2bd63a69c509
janis-jenny <pcardenasvm@gmail.com>
2021-09-30 20:21:18 -0500

    Hide api key with a enviroment variable

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
-export default axios.create({
+require('dotenv').config();
+
+const BASE_URL = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
 });
+/* eslint-disable  prefer-destructuring */
+const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
+// const API_KEY = 3694c3d2467f000d4597dfc9c1c650b8;
+
+export { BASE_URL, API_KEY };
--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
-import api from '../../Api';
+import { BASE_URL, API_KEY } from '../../Api';
 import * as action from './index';
+
+require('dotenv').config();
 
 /* const searchMovie = (payload) => ({
   type: ActionTypes.SEARCH_MOVIE,
@@ -21,7 +23,7 @@ const fetchMovie = () => async (dispatch) => {
   // const cleanSearchMovie = searchMovie.trim().toLowerCase();${cleanSearchMovie}``
   dispatch(action.movieSearchRequest());
   try {
-    const response = await api.get('/search/movie?api_key=3694c3d2467f000d4597dfc9c1c650b8&query=avengers');
+    const response = await BASE_URL.get(`/search/movie?api_key=${API_KEY}&query=avengers`);
     // console.log(response.data.results);
     dispatch(action.movieSearchSuccess(response.data.results));
   } catch (error) {
