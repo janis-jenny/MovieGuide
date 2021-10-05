c72ec826648729ebe0391bc09fab6d96e1f23b61
janis-jenny <pcardenasvm@gmail.com>
2021-10-05 00:45:00 -0500

    Add Box and Grid to Movielist, limit the number of lines in the paragraph

--- src/components/containers/MovieList.js
+++ src/components/containers/MovieList.js
@@ -1,3 +1,4 @@
+import { Box, Grid, GridItem } from '@chakra-ui/react';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import fetchMovie from '../../redux/actions/actionCreators';
@@ -12,12 +13,17 @@ const MovieList = () => {
     dispatch(fetchMovie());
   }, []);
 
-  // if (item.media_type === 'movie' && item.media_type === 'tv')
   const renderMovies = () => {
-    if (loading) return <Loader />;
+    if (loading) {
+      return (
+        <GridItem colSpan={5} className="my-5">
+          <Loader />
+        </GridItem>
+      );
+    }
     if (error) return <Error />;
 
-    return movies.filter((item) => item.media_type !== 'person').map((item) => (
+    return movies.filter((item) => item.media_type !== 'person' && item.title !== undefined).map((item) => (
       <div className="row d-flex" key={item.id}>
         <div className="col-4">
           <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`} alt="poster" />
@@ -26,7 +32,7 @@ const MovieList = () => {
           <li>
             <h4>{item.title}</h4>
             <span>{item.release_date}</span>
-            <p>{item.overview}</p>
+            <p className="paragraph">{item.overview}</p>
           </li>
         </ul>
       </div>
@@ -34,10 +40,15 @@ const MovieList = () => {
   };
 
   return (
-    <>
+    <Box width="60vw" mx="auto">
       <SearchField />
-      <div className="container">{renderMovies()}</div>
-    </>
+      <Grid
+        templateColumns=" 1fr"
+        gap={10}
+      >
+        {renderMovies()}
+      </Grid>
+    </Box>
   );
 };
 
--- src/components/shared/Loader.js
+++ src/components/shared/Loader.js
@@ -3,7 +3,7 @@ import { Center, Spinner } from '@chakra-ui/react';
 
 const Loader = () => (
   <Center>
-    <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />
+    <Spinner thickness="12px" speed="0.75s" emptyColor="gray.200" color="blue.500" size="xl" />
   </Center>
 );
 
